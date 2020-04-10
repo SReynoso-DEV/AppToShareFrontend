88028516b55758dbb3dfd01d3dbe71342e4af524
Sebastian Reynoso <sreynoso0499@gmail.com>
2020-04-10 17:54:02 -0500

    Sweetalert2 added and Login with axios

--- src/App.js
+++ src/App.js
@@ -1,54 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Swal from 'sweetalert2';
+
+const HttpsProxyAgent = require('https-proxy-agent');
 
 
 const Input = (props) => 
 <div class ="form-group">
-  <label for={props.name}>{props.name}</label><br/> 
-  <input class="form-control" type={props.type} placeholder={props.name} id={props.name}/>
+  <label name={props.name}>{props.name}</label><br/> 
+  <input class="form-control" type={props.type} placeholder={props.name} name={props.name} onChange={props.changeFunction}/>
 </div>
 
 const Button = (props) => <button type ={props.type} class="btn btn-primary">{props.value}</button>
 
-// class Text extends Component {
-//   render() {
-//     const flag = this.props.boolean ? "Es cierto!" : "Es falso!"
-//     const mappedNumbers = this.props.array.map(x => x * 2)
-//     return (
-//       <div>
-//         <p>{this.props.t1}</p>
-//         <p>{this.props.t2}</p>
-//         <p>{this.props.number}</p>
-//         <p>{this.props.number-1}</p>
-//         <p>{this.props.number+2}</p>
-//         <p>{this.props.number-5}</p>
-//         <p>{flag}</p>
-//         <p>{mappedNumbers.join(', ')}</p>
-//         <p>{this.props.login.userCode}</p>
-//         <p>{this.props.login.password}</p>
-//       </div>
-//     )
-//   }
-// }
 
 
-function App() {
-  return (
-    <div className="App">
+
+// AXIOS CONFIGURATION
+const axiosDefaultConfig = {
+    baseURL: 'https://upc-pool-ferluisxd.cloud.okteto.net',
+    proxy: false,
+    httpsAgent: new HttpsProxyAgent('http://localhost:8080')
+};
+
+const axios = require ('axios').create(axiosDefaultConfig);
+// END AXIOS CONFIGURATION
+
+class Index extends Component{ 
+  constructor(props){
+    super(props);
+    this.state = {
+      userCode: '',
+      password: ''
+    }
+  }
+
+  loginChange = (e) => {    
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  send = e => {
+    e.preventDefault()
+    axios.post('/api/auth/login/exp', this.state)
+    .then(response => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Login Confirm!',
+        html: `userCode: ${this.state.userCode}<br/>password: ${this.state.password}<br/>Token: ${response.data}`,
+        showConfirmButton: true})         
+    })
+    .catch(error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error :(',
+        html: `userCode: ${this.state.userCode}<br/>password: ${this.state.password}<br/>Mensaje: ${error.response.data.description.message}<br/>StatusCode: ${error.response.data.description.statusCode}`,
+        showConfirmButton: true})  
+    })
+  }
+
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form>
+        <form onSubmit = {this.send}>
           <Input type="text"
-                 name="userCode"/>
+                 name="userCode"
+                 changeFunction={this.loginChange}/>
           <Input type="password"
-                 name="password" />
+                 name="password"
+                 changeFunction={this.loginChange} />
           <Button type="submit"
-                value="Enviar"/>                 
+                value="Enviar" />                 
         </form>
       </header>
     </div>
-  );
+    )
+  }
+
+
 }
 
-export default App;
+export default Index;
