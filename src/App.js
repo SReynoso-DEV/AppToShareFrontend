33e289773e018f2507e35ae2f1ac16763028059a
Rodrigo Lozano <rolozanocampos@hotmail.com>
2020-04-10 02:24:15 -0500

    Login working

--- src/App.js
+++ src/App.js
@@ -6,44 +6,74 @@ import './App.css';
 const Hello = (props) => <h2>{props.title}</h2>
 
 class Text extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userCode: '',
+      password: ''
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  async login(e) {
+    e.preventDefault();
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const requestOptions = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ userCode: this.state.userCode, password: this.state.userCode})
+    }
+    const request = new Request('/api/auth/login/exp', requestOptions);
+    await fetch(request).then((resp)=>{ return resp.text() }).then((text)=>{ console.log(text) });
+    
+  }
+
   render() {
-    const flag = this.props.boolean ? "Es cierto!" : "Es falso!"
-    const mappedNumbers = this.props.array.map(x => x * 2)
-    return (
-      <div>
-        <p>{this.props.t1}</p>
-        <p>{this.props.t2}</p>
-        <p>{this.props.number}</p>
-        <p>{this.props.number-1}</p>
-        <p>{this.props.number+2}</p>
-        <p>{this.props.number-5}</p>
-        <p>{flag}</p>
-        <p>{mappedNumbers.join(', ')}</p>
-        <p>{this.props.login.userCode}</p>
-        <p>{this.props.login.password}</p>
-      </div>
+    const { userCode, password } = this.state;
+    return(
+        <form onSubmit ={this.submitHandler}>
+            <label >Usuario</label>
+            <input type="text" name="userCode" 
+                onChange={this.changeHandler}
+                value={userCode} />
+
+            <label>Password</label>
+            <input type="password" name="password" 
+                onChange={this.changeHandler} 
+                value={password} />
+
+            <button
+                type="submit"
+                className="btn btn-primary" onClick={e => this.login(e) }>Ingresar
+            </button>
+        </form>
     )
   }
 }
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello title="Hi from props!" />
+        <Hello title="Log In" />
         <Text
           t1="Esto es un texto implementado desde un const."
           t2="Recuerda en JSX solo puede haber un componente, por ende si deseas agrupar varios debes usar la etiqueta div."
           number={20}
           boolean={false}
           array={[1, 2, 3]}
-          login={{ userCode: 'u201713131', password: '*abcdef1234*' }}
         />
       </header>
     </div>
   );
 }
+  
 
 export default App;
